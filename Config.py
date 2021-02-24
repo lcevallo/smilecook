fd2a2f0948d4539b1616ac7645ffa83e3fa62e35
Luis Alberto Cevallos Cavero <lcevallo@gmail.com>
2021-02-24 16:33:44 -0500

    Initial commit

---
+++ Config.py
@@ -0,0 +1,3 @@
+class Config:
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lcevallo:PoiZxc357@localhost/smilecook'
---
+++ Readme.md
@@ -0,0 +1,2 @@
+para los colores
+https://coolors.co/b8336a-c695be-726da8-7d8cc4-a0d2db-b2b1d6-c490d1
